c4113e3e8abf8e32ff8aa5727ef35140e831f88a
harshvishu <mhacnagbani@gmail.com>
2017-02-21 17:46:05 +0530

    added navigation drawer stub code

--- app/src/main/java/com/brotherpowers/audiojournal/Main/MainActivity.java
+++ app/src/main/java/com/brotherpowers/audiojournal/Main/MainActivity.java
@@ -2,13 +2,19 @@ package com.brotherpowers.audiojournal.Main;
 
 import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.GravityCompat;
 import android.support.v4.view.ViewPager;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.LinearLayout;
 
 import com.brotherpowers.audiojournal.AudioRecorder.AudioRecorder;
@@ -24,7 +30,9 @@ import io.realm.Realm;
 
 
 public class MainActivity extends AppCompatActivity
-        implements AudioRecordingFragment.OnFragmentInteractionListener, RecordsFragment.OnFragmentInteractionListener {
+        implements AudioRecordingFragment.OnFragmentInteractionListener,
+        RecordsFragment.OnFragmentInteractionListener,
+        NavigationView.OnNavigationItemSelectedListener {
 
 
     /**
@@ -56,6 +64,15 @@ public class MainActivity extends AppCompatActivity
 
         realm = Realm.getDefaultInstance();
         setupTabLayout(realm);
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
     }
 
     private void setupTabLayout(Realm realm) {
@@ -66,9 +83,40 @@ public class MainActivity extends AppCompatActivity
         }
     }
 
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.nav_camera) {
+            // Handle the camera action
+        } else if (id == R.id.nav_gallery) {
+
+        } else if (id == R.id.nav_slideshow) {
+
+        } else if (id == R.id.nav_manage) {
+
+        } else if (id == R.id.nav_share) {
+
+        } else if (id == R.id.nav_send) {
+
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
     /*
-    * Recording Fragment Interface
-    * */
+        * Recording Fragment Interface
+        * */
     @Override
     public void onRecordingStateChange(AudioRecorder.STATE state) {
         isRecording = state == AudioRecorder.STATE.RECORDING;
